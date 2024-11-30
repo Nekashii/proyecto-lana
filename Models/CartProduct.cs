bc2b816d95676c8a12d12eabff14af8198293c34
Rebecca C <nekashii@outlook.com>
2024-11-30 01:30:40 -0600

    feat: update relations

--- Models/CartProduct.cs
+++ Models/CartProduct.cs
@@ -3,8 +3,8 @@ namespace Store.Models;
 public class CartProduct
 {
     public required int Id { get; set; }
-    public required int UserId { get; set; }
-    public required int ProductId { get; set; }
+    public required User User { get; set; }
+    public required Product Product { get; set; }
     public required int Amount { get; set; }
     public required DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime? UpdatedAt { get; set; }
--- Models/OrderProduct.cs
+++ Models/OrderProduct.cs
@@ -3,8 +3,7 @@ namespace Store.Models;
 public class OrderProduct
 {
     public required int Id { get; set; }
-    public required int OrderId { get; set; }
-    public required int ProductId { get; set; }
+    public required Product Product { get; set; }
     public required double Price { get; set; }
     public required int Amount { get; set; }
     public required DateTime CreatedAt { get; set; } = DateTime.Now;
